5cb79b4d6bb0eceeeb7eaeebb9a036fd788cf86b
Liam Treacy <liam.treacy@gmail.com>
2025-06-06 13:20:06 +0100

    WIP

--- tools/get_kube_logs.go
+++ tools/get_kube_logs.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"bufio"
 )
 
-func main()  {
+func GetKubeNS() ([]string, error) {
+	var kubeDefFile = "config/kubenamespaces"
+
+	file, err := os.Open(kubeDefFile)
+	if err != nil {
+		fmt.Println("err: ", err)
+		return nil, err
+	}
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	defer file.Close()
+	return lines, nil
+}
+
+func main() {
 	// Load in file with names of kubenamespaces of interest
 	// That file will not be saved to git
 	//
@@ -15,17 +33,10 @@ func main()  {
 	//  Get directory of caller
 	//	Get logs of selected kube (to calling dir)
 	dir, err := os.Getwd()
-	var kubeDefFile = "config/kubenamespaces"
-	file, err := os.Open(kubeDefFile)
+	kube_namespaces, err := GetKubeNS()
+	fmt.Println(kube_namespaces)
 
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
-
-	if err == nil{
-	fmt.Println("hello! you are calling from: ", dir)
+	if err == nil {
+		fmt.Println("hello! you are calling from: ", dir)
 	}
 }
